64b27f35162a45680aefd604cfe0bda39784525c
lmstrand <lmstrand@gmail.com>
2022-09-30 15:00:25 +0300

    KD-5605 Do not alert duplicate if trying to update hold ID already in use by logged in patron

--- Koha/Plugin/Fi/KohaSuomi/DI/PatronController.pm
+++ Koha/Plugin/Fi/KohaSuomi/DI/PatronController.pm
@@ -211,19 +211,26 @@ sub update {
             if ($changes->{othernames}){
                 my $othernamesmodreq = $changes->{othernames};
 
-                my $checkparams = {
-                    othernames => $othernamesmodreq
-                };
-                my $ok = 1;
-                my $change = {};
-                $change->{othernames} = $othernamesmodreq;
-            
-                $ok = 0 if Koha::Patrons->find($checkparams);
+                my $patron = Koha::Patrons->find( $patron_id );
+                my $old_othernames  = $patron->othernames;
 
-                if(!$ok) {
-                    return $c->render(status => 409, openapi => { error => "Duplicate othernames", conflict => $change });
+                if ($old_othernames ne $othernamesmodreq){
+
+                    my $checkparams = { 
+                    othernames => $othernamesmodreq
+                    };
+                    my $ok = 1;
+                    my $change = {}; 
+                    $change->{othernames} = $othernamesmodreq;
+    
+                    $ok = 0 if Koha::Patrons->find($checkparams);
+    
+                    if(!$ok) { 
+                        return $c->render(status => 409, openapi => { error => "Duplicate othernames", conflict => $change });
+
+                    }
                 }   
-            }
+            }   
             Koha::Patron::Modifications->search({ borrowernumber => $patron_id })->delete;
             Koha::Patron::Modification->new($changes)->store();
 
